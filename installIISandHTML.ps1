64c165ec123192b762073f668ed51654253adfca
StudentLoans999 <77641113+StudentLoans999@users.noreply.github.com>
2021-10-25 12:45:49 -0400

    Update installIISandHTML.ps1

--- installIISandHTML.ps1
+++ installIISandHTML.ps1
@@ -1,2 +1,4 @@
+import-module servermanager
+add-windowsfeature web-server -includeallsubfeature
 Add-WindowsFeature Web-Server
 Set-Content -Path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername) !"
