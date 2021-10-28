89a4a50455aef77ab3626ac8e2f0ade8ee72898d
StudentLoans999 <77641113+StudentLoans999@users.noreply.github.com>
2021-10-27 20:26:01 -0400

    Update P2Scertificates.ps1

--- P2Scertificates.ps1
+++ P2Scertificates.ps1
@@ -1,20 +1,6 @@
-$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature-Subject "CN=RootCertificate" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
 
--Subject "CN=RootCertificate" -KeyExportPolicy Exportable `
-
--HashAlgorithm sha256 -KeyLength 2048 `
-
--CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
-
-New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
-
--Subject "CN=ClientCertificate" -KeyExportPolicy Exportable `
-
--HashAlgorithm sha256 -KeyLength 2048 `
-
--CertStoreLocation "Cert:\CurrentUser\My" `
-
--Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature -Subject "CN=ClientCertificate" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
 
 # 1. Generate Root Certificate
 # 2. Generate Client Certificate
